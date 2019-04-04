0d6b06da7e91d26583d2c684998cf968e6527ee0
Viet-Anh, Nguyen <vietanh@vietanhdev.com>
2019-04-04 13:19:26 +0700

    Refactor things

--- ui/README.md
+++ ui/README.md
@@ -105,3 +105,9 @@ If you're looking for something more, check out [Shards Dashboard Pro React](htt
 ### Changelog
 
 Please check out the [CHANGELOG](CHANGELOG.md).
+
+
+
+## Icons
+
+<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
--- ui/src/components/layout/MainSidebar/SidebarMainNavbar.js
+++ ui/src/components/layout/MainSidebar/SidebarMainNavbar.js
@@ -35,12 +35,12 @@ class SidebarMainNavbar extends React.Component {
                 id="main-logo"
                 className="d-inline-block align-top mr-1"
                 style={{ maxWidth: "25px" }}
-                src={require("../../../images/shards-dashboards-logo.svg")}
-                alt="Shards Dashboard"
+                src={require("../../../images/horse.svg")}
+                alt="iCT Gaming Zone"
               />
               {!hideLogoText && (
                 <span className="d-none d-md-inline ml-1">
-                  Shards Dashboard
+                  iCT Gaming Zone
                 </span>
               )}
             </div>
