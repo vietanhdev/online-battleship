c5c647ad3b18c92f979f6713681a004ec948f9d8
Viet-Anh, Nguyen <vietanh@vietanhdev.com>
2019-04-06 17:53:18 +0700

    Remove search bar

--- ui/src/components/layout/MainSidebar/MainSidebar.js
+++ ui/src/components/layout/MainSidebar/MainSidebar.js
@@ -5,7 +5,6 @@ import classNames from "classnames";
 import { Col } from "shards-react";
 
 import SidebarMainNavbar from "./SidebarMainNavbar";
-import SidebarSearch from "./SidebarSearch";
 import SidebarNavItems from "./SidebarNavItems";
 
 
@@ -28,7 +27,6 @@ class MainSidebar extends React.Component {
         md={{ size: 3 }}
       >
         <SidebarMainNavbar hideLogoText={this.props.hideLogoText} />
-        <SidebarSearch />
         <SidebarNavItems />
       </Col>
     );
