fe613a81613dec8c0bc48ce2d622e4de8d672ec1
Sy-An Nguyen <syan224@gmail.com>
2019-04-07 19:04:28 +0700

    allow all cors

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 from .config import config_by_name
 
@@ -16,6 +17,7 @@ socketio = SocketIO()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
+    CORS(app)
     db.init_app(app)
     flask_bcrypt.init_app(app)
     socketio.init_app(app)
