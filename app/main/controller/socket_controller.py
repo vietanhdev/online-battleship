c5252356245a117fd8edfc765a74baec7da8f121
Sy-An Nguyen <syan224@gmail.com>
2019-04-07 18:47:19 +0700

    update path

--- app/main/controller/socket_controller.py
+++ app/main/controller/socket_controller.py
@@ -52,76 +52,7 @@ def registerUserId(request_object):
             }
 
     emit('response_login', response_object, broadcast=False, namespace='/')
-
-
-@socketio.on('request_login', namespace='/rooms')
-def registerUserRoomID(requet_object):
-    user, remain_sec = Auth.socket_logged_in_user(request_object)
-    room_public_id = requet_object.get('room_public_id')
-    room = get_a_room(room_public_id)
-
-    response_object = {
-        'status': 'fail',
-        'message': 'Fail to authenticate'
-    }
-
-    if room is None:
-        response_object = {
-            'status': 'fail',
-            'message': 'Room not found'
-        }
-    else:
-        if user is not None:
-            # save pair of session id with user id
-            result = save_user_id_room_id_with_sid(session_id=request.sid, user_id=user.id, room_id=room.id, remain_sec=remain_sec)
-
-            if result == True:
-                # join private room with specific name room is user with public id
-                join_room(room=user.public_id, namespace='/')
-                join_room(room=room_public_id, namespace='/rooms')
-                # Notify user is authenticate successfully
-                response_object = {
-                    'status': 'success',
-                    'message': 'Authenticate successfully'
-                }
-        
-    emit('response_login', response_object, broadcast=False, namespace='/rooms')
-        
-        
-
-# @socketio.on('request_join_room', namespace='/rooms')
-# def joinRoom(request_object):
-#     # check authentication session id
-#     user_id = get_user_id_by_sid(request.sid)
-#     user = get_a_user_by_id(user_id)
-#     if not user:
-#         response_object = {
-#             'status': 'fail',
-#             'message': 'Fail authenticate'
-#         }
-#         emit('response_join_room', response_object, broadcast=False, namespace='/rooms')
-#     else:
-#         # check if room exist
-#         room_public_id = request_object.get('room_public_id')
-#         private = is_private(room_public_id)
-#         if private is not False:
-#             response_object = {
-#                 'status': 'fail',
-#                 'message': 'Room not found'
-#             }
-#             emit('response_join_room', response_object, broadcast=False, namespace='/rooms')
-#         else:
-#             # join session id in room name is room public id
-#             join_room(room=room_public_id, namespace='/rooms')
-#             join_room(room=room_public_id, namespace='/messages')
-
-#             # Notify user is authenticate successfully
-#             response_object = {
-#                 'status': 'success',
-#                 'message': 'Join room successfully'
-#             }
-#             emit('response_join_room', response_object, broadcast=False, namespace='/rooms')
-
+    
 
 @socketio.on('request_private_message', namespace='/')
 def newPrivateMessage(request_object):
@@ -162,7 +93,6 @@ def newPrivateMessage(request_object):
         }
     
     emit('response_private_message', response_object, broadcast=False, namespace='/')
-
 
 
 # @socketio.on('request_message', namespace='/messages')
@@ -207,38 +137,112 @@ def newPrivateMessage(request_object):
 #             }
 #             emit('response_message', response_object, broadcast=False, namespace='/messages')
 
+@socketio.on('request_login_with_room', namespace='/rooms')
+def registerUserRoomID(request_object):
+    print(request_object)
+    user, remain_sec = Auth.socket_logged_in_user(request_object)
+    room_public_id = request_object.get('room_public_id')
+    room = get_a_room(room_public_id)
+
+    response_object = {
+        'status': 'fail',
+        'message': 'Fail to authenticate'
+    }
+
+    if room is None:
+        response_object = {
+            'status': 'fail',
+            'message': 'Room not found'
+        }
+    else:
+        if user is not None:
+            # save pair of session id with user id
+            result = save_user_id_room_id_with_sid(session_id=request.sid, user_id=user.id, room_id=room.id, remain_sec=remain_sec)
+
+            if result == True:
+                # join private room with specific name room is user with public id
+                join_room(room=user.public_id, namespace='/')
+                join_room(room=room_public_id, namespace='/rooms')
+                # Notify user is authenticate successfully
+                response_object = {
+                    'status': 'success',
+                    'message': 'Authenticate successfully'
+                }
+        
+    emit('response_login_with_room', response_object, broadcast=False, namespace='/rooms')
+
+
+@socketio.on('request_room_message', namespace='/rooms')
+def newRoomMessage(request_object):
+    # Check authenticate session id
+    user_id = get_user_id_by_sid(request.sid)
+    user = get_a_user_by_id(user_id)
+
+    room_id = get_room_id_by_sid(request.sid)
+    room = get_a_room_by_id(room_id)
+
+    content = request_object.get('content')
+
+    response_object = {
+        'status': 'false',
+        'message': 'Fail to authenticate'
+    }
+
+    if room is None:
+        response_object = {
+            'status': 'false',
+            'message': 'Room not found'
+        }
+    else:
+        # Save new message to database
+        save_new_message(sender_public_id=user.public_id, receiver_public_id=room.public_id, content=content)
+
+        # Send new message to all receiver in that room
+        receive_object = {
+            'sender_public_id': user.public_id,
+            'content': content
+        }
+        emit('receive_message', receive_object, room=room.public_id, namespace='/rooms')
+
+        # Notify sender that message is saved and sent successful
+        response_object = {
+            'status': 'success',
+            'message': 'Send message successfully'
+        }
+    
+    emit('response_room_message', response_object, broadcast=False, namespace='/rooms')
+
 
 @socketio.on('request_command', namespace='/rooms')
 def newCommand(request_object):
     # check authenticate session id
     user_id = get_user_id_by_sid(request.sid)
     user = get_a_user_by_id(user_id)
-    if not user:
+
+    room_id = get_room_id_by_sid(request.sid)
+    room = get_a_room_by_id(room_id)
+
+    command = json.loads(request_object.get('command'))
+
+    response_object = {
+        'status': 'false',
+        'message': 'Fail to authenticate'
+    }
+
+    if room is None:
         response_object = {
             'status': 'false',
-            'message': 'Fail authenticate'
+            'message': 'Room not found'
         }
-        emit('response_message', response_object, broadcast=False, namespace='/messages')
     else:
-        room_public_id = request_object.get('room_public_id')
-        print(room_public_id)
-        room = get_a_room(room_public_id)
-        command = json.loads(request_object.get('command'))
-        if room is None:
-            response_object = {
-                'status': 'fail',
-                'message': 'Room not found'
-            }
-            emit('response_command', response_object, broadcast=False, namespace='/rooms')
-        elif room.check_exist_player(user) == False:
+        if room.check_exist_player(user) == False:
             response_object = {
                 'status': 'fail',
                 'message': 'You are not player of this room'
             }
-            emit('response_command', response_object, broadcast=False, namespace='/rooms')
         else:
-            response_command, receive_event = send_command(user, room, command)
-            if response_command is not None:
-                emit('response_command', response_command, broadcast=False, namespace='/rooms')
+            response_object, receive_event = send_command(user, room, command)
             if receive_event is not None:
                 emit('receive_event', receive_event, room=room_public_id, namespace='/rooms')
+
+    emit('response_command', response_object, broadcast=False, namespace='/rooms')
