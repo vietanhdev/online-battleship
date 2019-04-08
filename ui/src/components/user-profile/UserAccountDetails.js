28679c216ef74e894a4693da25eeaaa7456cbff7
Dinh-Tuan Nguyen <tuannguyendinh224@gmail.com>
2019-04-08 12:17:53 +0700

    Implement User Login and User Profile

--- ui/src/components/user-profile/UserAccountDetails.js
+++ ui/src/components/user-profile/UserAccountDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 import PropTypes from "prop-types";
 import {
   Card,
@@ -15,126 +15,93 @@ import {
   Button
 } from "shards-react";
 
-const UserAccountDetails = ({ title }) => (
-  <Card small className="mb-4">
-    <CardHeader className="border-bottom">
-      <h6 className="m-0">{title}</h6>
-    </CardHeader>
-    <ListGroup flush>
-      <ListGroupItem className="p-3">
-        <Row>
-          <Col>
-            <Form>
-              <Row form>
-                {/* First Name */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feFirstName">First Name</label>
-                  <FormInput
-                    id="feFirstName"
-                    placeholder="First Name"
-                    value="Sierra"
-                    onChange={() => {}}
-                  />
-                </Col>
-                {/* Last Name */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feLastName">Last Name</label>
-                  <FormInput
-                    id="feLastName"
-                    placeholder="Last Name"
-                    value="Brooks"
-                    onChange={() => {}}
-                  />
-                </Col>
-              </Row>
-              <Row form>
-                {/* Email */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feEmail">Email</label>
-                  <FormInput
-                    type="email"
-                    id="feEmail"
-                    placeholder="Email Address"
-                    value="sierra@example.com"
-                    onChange={() => {}}
-                    autoComplete="email"
-                  />
-                </Col>
-                {/* Password */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="fePassword">Password</label>
-                  <FormInput
-                    type="password"
-                    id="fePassword"
-                    placeholder="Password"
-                    value="EX@MPL#P@$$w0RD"
-                    onChange={() => {}}
-                    autoComplete="current-password"
-                  />
-                </Col>
-              </Row>
-              <FormGroup>
-                <label htmlFor="feAddress">Address</label>
-                <FormInput
-                  id="feAddress"
-                  placeholder="Address"
-                  value="1234 Main St."
-                  onChange={() => {}}
-                />
-              </FormGroup>
-              <Row form>
-                {/* City */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feCity">City</label>
-                  <FormInput
-                    id="feCity"
-                    placeholder="City"
-                    onChange={() => {}}
-                  />
-                </Col>
-                {/* State */}
-                <Col md="4" className="form-group">
-                  <label htmlFor="feInputState">State</label>
-                  <FormSelect id="feInputState">
-                    <option>Choose...</option>
-                    <option>...</option>
-                  </FormSelect>
-                </Col>
-                {/* Zip Code */}
-                <Col md="2" className="form-group">
-                  <label htmlFor="feZipCode">Zip</label>
-                  <FormInput
-                    id="feZipCode"
-                    placeholder="Zip"
-                    onChange={() => {}}
-                  />
-                </Col>
-              </Row>
-              <Row form>
-                {/* Description */}
-                <Col md="12" className="form-group">
-                  <label htmlFor="feDescription">Description</label>
-                  <FormTextarea id="feDescription" rows="5" />
-                </Col>
-              </Row>
-              <Button theme="accent">Update Account</Button>
-            </Form>
-          </Col>
-        </Row>
-      </ListGroupItem>
-    </ListGroup>
-  </Card>
-);
 
-UserAccountDetails.propTypes = {
-  /**
-   * The component's title.
-   */
-  title: PropTypes.string
-};
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-UserAccountDetails.defaultProps = {
-  title: "Account Details"
-};
+export class UserAccountDetails extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
 
-export default UserAccountDetails;
+  render() {
+    const {fullname, email, bio} = this.props.user;
+    return (
+      <Card small className="mb-4">
+        <CardHeader className="border-bottom">
+          <h6 className="m-0">Details</h6>
+        </CardHeader>
+        <ListGroup flush>
+          <ListGroupItem className="p-3">
+            <Row>
+              <Col>
+                <Form>
+                  <Row form>
+                    {/* Full Name */}
+                    <Col md="12" className="form-group">
+                      <label htmlFor="feFirstName">Fullname</label>
+                      <FormInput
+                        id="feFirstName"
+                        placeholder="First Name"
+                        value={fullname}
+                      />
+                    </Col>
+                  </Row>
+                  <Row form>
+                    {/* Email */}
+                    <Col md="12" className="form-group">
+                      <label htmlFor="feEmail">Email</label>
+                      <FormInput
+                        type="email"
+                        id="feEmail"
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={() => {}}
+                        autoComplete="email"
+                      />
+                    </Col>
+                    {/* Password */}
+                    <Col md="6" className="form-group">
+                      <label htmlFor="fePassword1">Old Password</label>
+                      <FormInput
+                        type="password"
+                        id="fePassword1"
+                        placeholder="Password"
+                      />
+                    </Col>
+                    <Col md="6" className="form-group">
+                      <label htmlFor="fePassword2">New Password</label>
+                      <FormInput
+                        type="password"
+                        id="fePassword2"
+                        placeholder="New password"
+                      />
+                    </Col>
+                  </Row>
+                  <Row form>
+                    {/* Description */}
+                    <Col md="12" className="form-group">
+                      <label htmlFor="feDescription">Bio</label>
+                      <FormTextarea id="feDescription" rows="5" />
+                    </Col>
+                  </Row>
+                  <Button theme="accent">Update Account</Button>
+                </Form>
+              </Col>
+            </Row>
+          </ListGroupItem>
+        </ListGroup>
+      </Card>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  user: state.userReducer
+})
+
+const mapDispatchToProps = {
+  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserAccountDetails)
