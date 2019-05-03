f3083a7af145a2205566c801f273f03360441554
Dinh-Tuan Nguyen <vietanh@vietanhdev.com>
2019-05-03 10:41:53 +0700

    Friend list UI design

--- ui/src/components/friends/Friend.js
+++ ui/src/components/friends/Friend.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Button } from 'shards-react'
+
+const online = require('../../images/friends/online.jpg')
+const offline = require('../../images/friends/offline.jpg')
 
 export class Friend extends Component {
 
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      showActionButtons: false
+    }
+  }
+
   render() {
+    const friend = this.props.data;
     return (
-        <div style={{ border: "1px solid #ddd", height: "3rem", padding: "0.4rem", display: "flex" }}>
-            <img alt="Avatar" style={{width: "2rem", height: "2rem" }} src={require("../../images/avatars/king.png")}></img>
-            <p style={{fontSize: "1rem", lineHeight:" 2rem", marginLeft: "2rem"}} >Viet Anh</p>
+        <div onMouseEnter={() => this.setState({ showActionButtons: true })}
+        onMouseLeave={() => this.setState({ showActionButtons: false })} style={{ borderBottom: "1px solid #ddd", height: "3rem", padding: "0.4rem", display: "flex", position: "relative"}}>
+            <img alt={friend.image} style={{width: "2rem", height: "2rem" }} src={friend.image}></img>
+            <p style={{fontSize: "1rem", lineHeight:" 2rem", marginLeft: "2rem"}} >{friend.name}</p>
+            <div style={{ marginLeft: "1rem", display: this.state.showActionButtons ? "block" : "none"}}>
+              <Button theme="light" size="sm" style={{ marginLeft: "0.1rem"}}>
+                <i className="material-icons">mail</i>
+              </Button>
+              <Button theme="light" size="sm" style={{ marginLeft: "0.1rem"}}>
+                Play Battleship
+              </Button>
+            </div>
+            <img alt="status" src={friend.online? online : offline} style={{position: "absolute", top: "1rem", right: "1rem"}}></img>
         </div>
     )
   }
--- ui/src/redux/friends/reducers.js
+++ ui/src/redux/friends/reducers.js
@@ -2,13 +2,134 @@ import friendConstants from './constants'
 import {notifierActions} from '../notifier/actions'
 
 const initState = {
-    friendList : []
+    friends: [
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        },
+        {
+            name: "Viet Anh",
+            image: require('../../images/avatars/king.png'),
+            online: true
+        },
+        {
+            name: "Sy An",
+            image: require('../../images/avatars/king.png'),
+            online: false
+        }
+    ]
 }
 
 export const friendReducer = (state = initState, action) =>  {
     switch (action.type) {
         case friendConstants.FETCH_FRIENDS_SUCCESS:
-            return {...state, friendList: action.payload}
+            return {...state, friends: action.payload}
         default:
             return state
     }
