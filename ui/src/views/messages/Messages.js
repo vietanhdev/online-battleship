c4adc2c3b66e2a05322cfa82fe01081bc090a7b8
Viet-Anh, Nguyen <vietanh@vietanhdev.com>
2019-05-14 11:09:33 +0700

    Remove header

--- ui/src/views/messages/Messages.js
+++ ui/src/views/messages/Messages.js
@@ -123,11 +123,6 @@ class Messages extends React.Component {
         return (
 
           <Container fluid className="main-content-container px-4">
-            {/* Page Header */}
-            <Row noGutters className="page-header py-4">
-              <PageTitle sm="4" title="Games" subtitle="Let's play with your friends!" className="text-sm-left" />
-            </Row>
-
             <Row>
               <Col md="9">
                 <Row>
