374f0ede622c9fa18b474767e3d857ba8dfe90c0
Viet-Anh, Nguyen <vietanh@vietanhdev.com>
2019-05-15 22:49:49 +0700

    Implement register page

--- ui/src/redux/user/reducers.js
+++ ui/src/redux/user/reducers.js
@@ -1,15 +1,24 @@
 import userConstants from './constants'
+import {notifierActions} from '../notifier/actions'
 
 const initState = {
-    
+    fullname: "",
+    email: "",
+    bio: "The best gamer on the Earth.",
+    isLoggedIn: false,
+    authKey: ""
 }
 
-export const userReducer = (state = initState, action) => {
+export const userReducer = (state = initState, action) => dispatch => {
     switch (action.type) {
-        case userConstants.NOTIFY_SUCCESS:
-            return {
-                message: action.payload
-            }
+        case userConstants.REGISTER_SUCCESS:
+            notifierActions.dismissAlert();
+            notifierActions.showMessage("Successfully registered!");
+            return state;
+        case userConstants.REGISTER_FAIL:
+            notifierActions.dismissAlert();
+            notifierActions.showError(action.message);
+            return state;
         default:
             return state
     }
