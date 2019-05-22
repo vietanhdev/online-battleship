d3ed33498564153d019d1bf07a0a7ffc6b9eb42a
Viet-Anh, Nguyen <vietanh@vietanhdev.com>
2019-05-22 19:37:35 +0700

    Hide avatar in chatbox

--- ui/src/views/messages/MessageItem.js
+++ ui/src/views/messages/MessageItem.js
@@ -4,7 +4,7 @@ export default class messageItem extends React.Component {
     render () {
         return (
             <li className={this.props.user? "message right appeared": "message left appeared"}>
-                <div className="avatar"></div>
+                {/* <div className="avatar"></div> */}
                 <div className="text_wrapper">
                     <div className="text">{this.props.message}</div>
                 </div>
