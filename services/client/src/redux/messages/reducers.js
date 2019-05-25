e9b8a8347b78e5564031c899afd45b93d5966730
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2019-05-25 10:55:07 +0700

    Refactor Messages component using redux

--- services/client/src/redux/messages/reducers.js
+++ services/client/src/redux/messages/reducers.js
@@ -1,10 +1,17 @@
-// import messageConstants from './constants'
+import messageConstants from './constants'
 
 const initState = {
+    messages: []
 }
 
 export const messageReducer = (state = initState, action) =>  {
     switch (action.type) {
+        case messageConstants.CLEAR_MESSAGES:
+            return {...state, messages: []}
+        case messageConstants.FETCH_MESSAGES_SUCCESS:
+            return {...state, messages: action.payload}
+        case messageConstants.PUSH_NEW_MESSAGE:
+            return {...state, messages: [...state.messages, action.payload]}
         default:
             return state
     }
