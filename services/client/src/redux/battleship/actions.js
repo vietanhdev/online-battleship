855a959a9445bde668c564ca681d9caa4ccc6abd
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2019-05-30 09:32:19 +0700

    Use more sounds in game

--- services/client/src/redux/battleship/actions.js
+++ services/client/src/redux/battleship/actions.js
@@ -1,11 +1,14 @@
 import request, {
     requestStatus
 } from '../../utilities/http'
-import battleshipConstants, {MessageEvent} from './constants'
+import battleshipConstants, {MessageEvent, BoardState} from './constants'
 
 import {
     notifierActions
 } from '../notifier/actions'
+
+
+import {getInitState} from './reducers'
 
 
 import Utilities from './utilities'
@@ -196,25 +199,194 @@ export const battleshipActions = {
     } ,
 
     // Update game state
-    updateGameState: (gameState) => (dispatch, getState) => {
+    updateGameState: (data) => (dispatch, getState) => {
 
-        // Play sound on game over
-        let currentGameState = getState().battleshipReducer;
-        if (gameState.game_over && !currentGameState.gameState.gameOver){
+        let currentState = getState().battleshipReducer;
+
+        // ========== Parse data from server response ==========
+        let myId = data.my_public_id;
+        let boards = data.boards;
+        let amIPlayer = data.is_player;
+        let player1 = getInitState().gameState.player1;
+        let player2 = getInitState().gameState.player2;
+        let players = data.room_data.players;
+        let turn = {};
+        for (let i = 0; i < boards.length; ++i) {
+            let board = boards[i];
+            let userId = board.user_public_id;
+            let playerInfo = {};
+            for (let j = 0; j < players.length; ++j) {
+                if (players[j].public_id === userId) {
+                    playerInfo = players[j];
+                    break;
+                }
+            }
+
+            let ships = [];
+            for (let i = 0; i < board.ships.length; ++i) {
+                ships.push({
+                    x: board.ships[i].x,
+                    y: board.ships[i].y,
+                    vertical: board.ships[i].vertical,
+                    sink: board.ships[i].sink,
+                    size: Utilities.shipLength2Size(board.ships[i].len_ship),
+                });
+            }
+
+
+            // If I am a player, I am the player1, otherwise player with i = 0 will be assigned as player1
+            let isPlayer1 = board.user_public_id === myId || (!amIPlayer && i === 0);
+            if ( isPlayer1 ) {
+
+                // Only update ships of player 1 when the shipsReady changed from false to true.
+                // This prevent the board from reseting when player 2 finishes the arrangement before player 1
+                let updatedArrangement = board.ships_ready && !currentState.gameState.player1.shipsReady;
+                player1 = {
+                    fullname: playerInfo.username,
+                    playerPublicId: playerInfo.public_id, 
+                    data: board.board,
+                    ships: updatedArrangement ? ships : currentState.gameState.player1.ships,
+                    shipsReady: board.ships_ready
+                };
+                
+            } else {
+                player2 = {
+                    fullname: playerInfo.username,
+                    playerPublicId: playerInfo.public_id, 
+                    data: board.board,
+                    ships: ships,
+                    shipsReady: board.ships_ready
+                };
+            }
+        }
+
+        // The player playing this turn
+        for (let j = 0; j < players.length; ++j) {
+            if (players[j].public_id === data.turn) {
+                turn = {
+                    fullname: players[j].username,
+                    playerPublicId: players[j].public_id,
+                }
+            }
+        }
+
+
+        let newState = {...currentState, 
+            myPublicId: myId,
+            gameState: {
+                ...currentState.gameState,
+                isInitialState: false,
+                gameOver: data.game_over,
+                winnerId: data.winner,
+                isEnoughPlayer: data.is_enough_player,
+                isMyRoom: data.is_player,
+                gameName: data.room_data.game.name,
+                roomId: data.room_data.room_public_id,
+                isMyTurn: data.turn === myId,
+                turn: turn,
+                player1: player1,
+                player2: player2
+            }
+        }
+
+        dispatch({
+            type: battleshipConstants.UPDATE_GAME_STATE,
+            payload: newState
+        });
+
+
+        // Play win or lose sound
+        if (newState.gameState.gameOver && !currentState.gameState.gameOver) {
+
+            // I am the loser
+            if (newState.gameState.isMyRoom && currentState.gameState.isMyRoom && newState.myPublicId !== newState.gameState.winnerId) {
+                dispatch({
+                    type: 'PLAY_SOUND',
+                    meta: {
+                        sound: {
+                            play :'lose'
+                        }
+                    }
+                })
+            } else { // Winner or not my game
+                dispatch({
+                    type: 'PLAY_SOUND',
+                    meta: {
+                        sound: {
+                            play :'win'
+                        }
+                    }
+                })
+            }
+        }
+
+        // Sound for shots
+        if (!currentState.gameState.isInitialState) {
+
+            // Player 1
+            let player1Board = currentState.gameState.player1.data;
+            let newPlayer1Board = newState.gameState.player1.data;
+            dispatch(battleshipActions.playShotSound(player1Board, newPlayer1Board));
+
+            // Player 2
+            let player2Board = currentState.gameState.player2.data;
+            let newPlayer2Board = newState.gameState.player2.data;
+            dispatch(battleshipActions.playShotSound(player2Board, newPlayer2Board));
+
+        }
+        
+        // New game started
+        if (newState.gameState.player1.shipsReady && newState.gameState.player2.shipsReady && (!currentState.gameState.player1.shipsReady || !currentState.gameState.player2.shipsReady)) {
             dispatch({
                 type: 'PLAY_SOUND',
                 meta: {
                     sound: {
-                        play :'win'
+                        play :'gameStarted'
                     }
                 }
             })
         }
 
-        dispatch({
-            type: battleshipConstants.UPDATE_GAME_STATE,
-            payload: gameState
-        });
+        
+    },
+
+
+
+    playShotSound: (oldBoard, newBoard)  => (dispatch, getState) => {
+
+        let rows = getState().battleshipReducer.gameState.boardHeight;
+        let cols = getState().battleshipReducer.gameState.boardWidth;
+
+        for (let y = 0; y < rows; ++y ) {
+            for (let x = 0; x < cols; ++x) {
+                try {
+
+                    if (oldBoard[y][x] === BoardState.HIDDEN && newBoard[y][x] < BoardState.HIDDEN) { // miss
+                        dispatch({
+                            type: 'PLAY_SOUND',
+                            meta: {
+                                sound: {
+                                    play :'fire'
+                                }
+                            }
+                        })
+                    } else if (oldBoard[y][x] === BoardState.HIDDEN && newBoard[y][x] > BoardState.HIDDEN) { // hit
+                        dispatch({
+                            type: 'PLAY_SOUND',
+                            meta: {
+                                sound: {
+                                    play :'wounded'
+                                }
+                            }
+                        })
+                    }
+
+                } catch(e) {
+                    console.log(e);
+                }
+            }
+        }
+
     },
 
 
--- services/client/src/redux/battleship/reducers.js
+++ services/client/src/redux/battleship/reducers.js
@@ -2,7 +2,7 @@ import battleshipConstants, {ShipSize} from './constants'
 import Utilities from './utilities';
 
 
-const getInitState = () => {
+export const getInitState = () => {
     let rowInit = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     let boardInitState = [rowInit, rowInit, rowInit, rowInit, rowInit, rowInit, rowInit, rowInit, rowInit, rowInit]
     const initState = {
@@ -12,6 +12,7 @@ const getInitState = () => {
             selectedShipSize: ShipSize.GIANT
         },
         gameState: {
+            isInitialState: true,
             gameOver: false,
             winnerId: "",
             boardWidth: 10,
@@ -87,95 +88,7 @@ export const battleshipReducer = (state = getInitState(), action) =>  {
                 }
             }
         case battleshipConstants.UPDATE_GAME_STATE:
-
-            // ========== Parse data from server response ==========
-            let myId = action.payload.my_public_id;
-            let boards = action.payload.boards;
-            let amIPlayer = action.payload.is_player;
-            let player1 = getInitState().gameState.player1;
-            let player2 = getInitState().gameState.player2;
-            let players = action.payload.room_data.players;
-            let turn = {};
-            for (let i = 0; i < boards.length; ++i) {
-                let board = boards[i];
-                let userId = board.user_public_id;
-                let playerInfo = {};
-                for (let j = 0; j < players.length; ++j) {
-                    if (players[j].public_id === userId) {
-                        playerInfo = players[j];
-                        break;
-                    }
-                }
-
-                let ships = [];
-                for (let i = 0; i < board.ships.length; ++i) {
-                    ships.push({
-                        x: board.ships[i].x,
-                        y: board.ships[i].y,
-                        vertical: board.ships[i].vertical,
-                        sink: board.ships[i].sink,
-                        size: Utilities.shipLength2Size(board.ships[i].len_ship),
-                    });
-                }
-
-
-                // If I am a player, I am the player1, otherwise player with i = 0 will be assigned as player1
-                let isPlayer1 = board.user_public_id === myId || (!amIPlayer && i === 0);
-                if ( isPlayer1 ) {
-
-                    // Only update ships of player 1 when the shipsReady changed from false to true.
-                    // This prevent the board from reseting when player 2 finishes the arrangement before player 1
-                    let updatedArrangement = board.ships_ready && !state.gameState.player1.shipsReady;
-                    player1 = {
-                        fullname: playerInfo.username,
-                        playerPublicId: playerInfo.public_id, 
-                        data: board.board,
-                        ships: updatedArrangement ? ships : state.gameState.player1.ships,
-                        shipsReady: board.ships_ready
-                    };
-                    
-                } else {
-                    player2 = {
-                        fullname: playerInfo.username,
-                        playerPublicId: playerInfo.public_id, 
-                        data: board.board,
-                        ships: ships,
-                        shipsReady: board.ships_ready
-                    };
-                }
-            }
-
-            // The player playing this turn
-            for (let j = 0; j < players.length; ++j) {
-                if (players[j].public_id === action.payload.turn) {
-                    turn = {
-                        fullname: players[j].username,
-                        playerPublicId: players[j].public_id,
-                    }
-                }
-            }
-
-
-            newState = {...state, 
-                myPublicId: myId,
-                gameState: {
-                    ...state.gameState,
-                    gameOver: action.payload.game_over,
-                    winnerId: action.payload.winner,
-                    isEnoughPlayer: action.payload.is_enough_player,
-                    isMyRoom: action.payload.is_player,
-                    gameName: action.payload.room_data.game.name,
-                    roomId: action.payload.room_data.room_public_id,
-                    isMyTurn: action.payload.turn === myId,
-                    turn: turn,
-                    player1: player1,
-                    player2: player2
-                }
-            }
-
-            console.log(newState)
-
-            return newState
+            return action.payload;
         default:
             return state
     }
